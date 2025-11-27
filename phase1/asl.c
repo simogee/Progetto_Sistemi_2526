a7b2e9385e0f4b9c396ad320da04bbd1d56b9b27
Alg3rn0n <alg3rn0n@Alg3rn0n.Bmo>
2025-11-27 09:03:13 +0100

    aggiunti commenti a insertBlocked

--- phase1/asl.c
+++ phase1/asl.c
@@ -21,8 +21,26 @@ void initASL() {
 
 /**aggiunge in coda al semaforo associato alla key semAdd. 
  * Se non c'è corrispondenza nella lista semd_h si inizializza un nuovo semaforo con key semAdd.
+ * idea: individuo la coda associata al valore semAdd. e faccio s_procq.prev per andare in coda aggiungendo l'ultimo elemento
+ * saltando così tutta la coda -> non sicuro: dipende cosa considerano come testa e cosa come coda.
  */
 int insertBlocked(int* semAdd, pcb_t* p) {
+
+        struct list_head *iter;
+        list_for_each(iter,semd_h){
+            semd_t *sem = container_of(iter,semd_t,s_link);
+            if ( sem == semAdd)
+                {
+                    list_add(..,sem->s_procq);
+                    //return qualcosa;
+                }
+        }
+        /**non ha trovato corrispondenza e va inizializzato il semaforo con la sua key associata
+         * 
+        */
+
+    
+
 }
 
 /** ricerca corrispondenza key, se non c'è NULL
